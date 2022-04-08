c8d796f915c1920910e43e68dd7292d69106fe18
Shivansh Sharma <shivansh.s@thinkbridge.in>
2022-04-08 17:16:56 +0530

    3500 tuple datatypes

---
+++ Datatypes/tuples.js
@@ -0,0 +1,4 @@
+var student = [32, "Shivansh"];
+console.log(student);
+var students = [32, "Shivansh", true, "Sharma", 7710352296];
+console.log(students);
---
+++ Datatypes/tuples.ts
@@ -0,0 +1,4 @@
+var student: [number,string] = [32,"Shivansh"];
+console.log(student);
+var students: [number,string,boolean,string,number] = [32,"Shivansh",true,"Sharma",7710352296];
+console.log(students);
