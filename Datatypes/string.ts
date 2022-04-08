e5b76c3d4837f15d94deac651b11472967dafd71
Shivansh Sharma <shivansh.s@thinkbridge.in>
2022-04-08 15:46:06 +0530

    3497 string datatype

---
+++ Datatypes/string.ts
@@ -0,0 +1,23 @@
+
+let myName: string = 'Shivansh Sharma';
+let yourName: string = "Sunny";
+
+console.log(myName);
+console.log(yourName);
+
+console.log(myName.charAt(0));
+console.log(myName.charAt(1));
+console.log(myName.charAt(4));
+
+console.log(yourName.charAt(0));
+console.log(yourName.charAt(1));
+console.log(yourName.charAt(4));
+
+console.log(myName.concat(yourName));
+
+console.log(yourName.indexOf('u'));
+
+console.log(yourName.replace('Sunny','Sunny Sharma'));
+
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
