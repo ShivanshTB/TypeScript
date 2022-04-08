3b875ff1859ecc161e8160df09890426044437dd
Shivansh Sharma <shivansh.s@thinkbridge.in>
2022-04-08 17:19:15 +0530

    3503 any datatype

---
+++ Datatypes/any.js
@@ -0,0 +1,11 @@
+var sayHi = "Hi";
+console.log(sayHi);
+sayHi = 32;
+console.log(sayHi);
+sayHi = true;
+console.log(sayHi);
+//any array
+var arr = ["Shivansh", 7710, true];
+console.log(arr);
+arr.push("Sharma");
+console.log(arr);
---
+++ Datatypes/any.ts
@@ -0,0 +1,16 @@
+let sayHi: any = "Hi";
+console.log(sayHi);
+
+sayHi = 32;
+console.log(sayHi);
+
+sayHi = true;
+console.log(sayHi);
+
+//any array
+
+let arr: any[] = ["Shivansh",7710,true];
+console.log(arr);
+
+arr.push("Sharma");
+console.log(arr);
