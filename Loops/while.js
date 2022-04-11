285dc7971d25638e17566fa70931db933cfcb71b
Shivansh Sharma <shivansh.s@thinkbridge.in>
2022-04-11 17:13:22 +0530

    3514 while loop

---
+++ Loops/while.js
@@ -0,0 +1,7 @@
+//while loop
+// The while loop is another type of loop that checks for a specified condition before beginning to execute the block of statements. The loop runs until the condition value is met.
+var i = 2;
+while (i < 4) {
+    console.log("Block statement execution no." + i);
+    i++;
+}
---
+++ Loops/while.ts
@@ -0,0 +1,9 @@
+//while loop
+// The while loop is another type of loop that checks for a specified condition before beginning to execute the block of statements. The loop runs until the condition value is met.
+
+let i: number = 2;
+
+while (i < 4) {
+    console.log( i )
+    i++;
+}
