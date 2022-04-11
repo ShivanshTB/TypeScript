c0f738b80c16d3dcbcf7dcf2893c04f6443e0d7c
Shivansh Sharma <shivansh.s@thinkbridge.in>
2022-04-11 15:10:13 +0530

    3513 for in loop

--- Loops/for loop.js
+++ Loops/for loop.js
@@ -9,3 +9,10 @@ for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
     var val = arr_1[_i];
     console.log(val);
 }
+//for in loop
+// The for...in loop iterates through a list or collection and returns an index on each iteration.
+var num = [10, 20, 30, 40];
+for (var index in num) {
+    console.log("Index: " + index);
+    console.log("Value: " + num[index]);
+}
--- Loops/for loop.ts
+++ Loops/for loop.ts
@@ -13,3 +13,14 @@ let arr :Array<number>= [10, 20, 30, 40];
 for (var val of arr) {
   console.log(val);
 }
+
+//for in loop
+// The for...in loop iterates through a list or collection and returns an index on each iteration.
+
+let num: Array<number> = [10, 20, 30, 40];
+
+for (var index in num) {
+  console.log("Index: "+index);
+
+  console.log("Value: "+num[index]);
+}
