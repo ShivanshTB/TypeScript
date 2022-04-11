79bdef3229c5a340176276413ea6a3646a572208
Shivansh Sharma <shivansh.s@thinkbridge.in>
2022-04-11 14:55:54 +0530

    3507 if else statement

---
+++ Control Statement/ifElse.js
@@ -0,0 +1,7 @@
+var num1 = 30, num2 = 50;
+if (num1 < num2) {
+    console.log("".concat(num1, " is less than ").concat(num2));
+}
+else {
+    console.log("".concat(num1, " is greater then ").concat(num2));
+}
---
+++ Control Statement/ifElse.ts
@@ -0,0 +1,8 @@
+let num1: number = 30, num2: number= 50;
+
+if (num1 < num2) 
+{
+    console.log(`${num1} is less than ${num2}`);
+}else{
+    console.log(`${num1} is greater then ${num2}`);
+}
