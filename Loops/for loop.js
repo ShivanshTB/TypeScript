0cff20f28cee26480677431679a29e4fd1b4c148
Shivansh Sharma <shivansh.s@thinkbridge.in>
2022-04-11 15:01:26 +0530

    3511 for loop

---
+++ Loops/for loop.js
@@ -0,0 +1,6 @@
+// for loop
+// for of loop
+// for in loop
+for (var i = 0; i < 3; i++) {
+    console.log("Number: " + i);
+}
---
+++ Loops/for loop.ts
@@ -0,0 +1,5 @@
+// for loop
+
+for (let i = 0; i < 3; i++) {
+    console.log ("Number: " + i);
+}  
