9f2b944c279b853607452a01a5a19b85a0874ec7
Shivansh Sharma <shivansh.s@thinkbridge.in>
2022-04-11 14:53:57 +0530

    3506 if statement

---
+++ Control Statement/if statement.js
@@ -0,0 +1,4 @@
+var num1 = 10, num2 = 20;
+if (num1 < num2) {
+    console.log("".concat(num1, " is less than ").concat(num2));
+}
---
+++ Control Statement/if statement.ts
@@ -0,0 +1,6 @@
+let num1: number = 10, num2: number= 20;
+
+if (num1 < num2) 
+{
+    console.log(`${num1} is less than ${num2}`);
+}
