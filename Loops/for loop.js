dac3411f14322efe9da135a5fda71beb89c0b8f8
Shivansh Sharma <shivansh.s@thinkbridge.in>
2022-04-11 15:06:01 +0530

    3512 for of loop

--- Loops/for loop.js
+++ Loops/for loop.js
@@ -1,6 +1,11 @@
 // for loop
-// for of loop
-// for in loop
 for (var i = 0; i < 3; i++) {
     console.log("Number: " + i);
+}
+// for of loop
+// for...of loop to iterate and access elements of an array, list, or tuple collection.
+var arr = [10, 20, 30, 40];
+for (var _i = 0, arr_1 = arr; _i < arr_1.length; _i++) {
+    var val = arr_1[_i];
+    console.log(val);
 }
--- Loops/for loop.ts
+++ Loops/for loop.ts
@@ -3,3 +3,13 @@
 for (let i = 0; i < 3; i++) {
     console.log ("Number: " + i);
 }  
+
+
+// for of loop
+// for...of loop to iterate and access elements of an array, list, or tuple collection.
+
+let arr :Array<number>= [10, 20, 30, 40];
+
+for (var val of arr) {
+  console.log(val);
+}
