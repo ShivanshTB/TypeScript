50b5dab4d9e51b6d5c68da77c9b5904289ea25a6
Shivansh Sharma <shivansh.s@thinkbridge.in>
2022-04-11 17:14:13 +0530

    3515 do while loop

---
+++ Loops/do while.js
@@ -0,0 +1,7 @@
+//do while
+// The do..while loop runs the block of code at least once before checking for the specified condition.
+var num = 2;
+do {
+    console.log(num);
+    num++;
+} while (num < 4);
---
+++ Loops/do while.ts
@@ -0,0 +1,8 @@
+//do while
+// The do..while loop runs the block of code at least once before checking for the specified condition.
+
+let num: number = 2;
+do {
+    console.log(num )
+    num++;
+} while ( num < 4)
