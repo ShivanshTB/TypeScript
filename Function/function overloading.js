6fe74b449dd69f0ece806e5332076ec75bcf1c7f
Shivansh Sharma <shivansh.s@thinkbridge.in>
2022-04-12 16:22:24 +0530

    3520 function overloading

---
+++ Function/function overloading.js
@@ -0,0 +1,9 @@
+function add(x, y) {
+    console.log(x + y);
+    return x + y;
+}
+add(2, 4);
+function add(x, y) {
+    console.log(x + y);
+}
+add("3", "7");
---
+++ Function/function overloading.ts
@@ -0,0 +1,9 @@
+function add(x: number, y:number): number{
+    console.log(x+y);
+    return x+y;
+}
+add(2,4);
+function add(x: string, y:string): void{
+    console.log(x+y);
+}
+add("3","7");
