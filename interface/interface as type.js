d31030b75897f741e6ec96e9f91ce971f3653aa1
Shivansh <shivansh.s@thinkbridge.in>
2022-04-19 11:04:48 +0530

    3551 interface as type

---
+++ interface/interface as type.js
@@ -0,0 +1,3 @@
+var kv1 = { key: 1, value: "Steve" };
+var kv2 = { key: 1, value: "Steve" };
+// let kv3: KeyPair = { key:1, value:100 };
---
+++ interface/interface as type.ts
@@ -0,0 +1,10 @@
+interface KeyPair {
+  key: number;
+  value: string;
+}
+
+let kv1: KeyPair = { key: 1, value: "Steve" };
+
+let kv2: KeyPair = { key: 1, value: "Steve" };
+
+// let kv3: KeyPair = { key:1, value:100 };
